457b30c6643a2c48b7c1cf857370439c5ae69e60
kingwar66 <kingwar66@gmail.com>
2021-03-24 22:54:41 +0200

    add react router dom router to app

--- src/Container/App.jsx
+++ src/Container/App.jsx
@@ -1,8 +1,9 @@
-import { Footer, Navbar } from '../components';
+import { Footer, Navbar, Home, Shop, NotFound } from '../components';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { unstable_createMuiStrictModeTheme as createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import Home from '../components/Pages/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 const theme = createMuiTheme({
     palette: {
@@ -17,16 +18,26 @@ const theme = createMuiTheme({
 
 const App = () => {
     return (
-        <>
+        <Router>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <Navbar />
                 <main>
-                    <Home />
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/shop">
+                            <Shop />
+                        </Route>
+                        <Route path="*">
+                            <NotFound />
+                        </Route>
+                    </Switch>
                 </main>
                 <Footer />
             </ThemeProvider>
-        </>
+        </Router>
     )
 }
 
