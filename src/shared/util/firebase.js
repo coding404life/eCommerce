05af9e45eebaabfaaefe9af8289c1f251538be4c
kingwar66 <kingwar66@gmail.com>
2021-09-16 18:18:01 +0200

    added firebase SDK to firebase.js and init inside app file

---
+++ src/shared/util/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAKacoCVILsLiRrE_e_nLhY75S5LzUmam4",
+  authDomain: "eren-commerce.firebaseapp.com",
+  projectId: "eren-commerce",
+  storageBucket: "eren-commerce.appspot.com",
+  messagingSenderId: "929457248222",
+  appId: "1:929457248222:web:b685529096402f03a7af4f",
+  measurementId: "G-PQPNP9YLG6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firebaseAnlytics = getAnalytics(app);
